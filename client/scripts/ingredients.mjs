76f75d4287e2db4bcea24dd9a86f841efce603f7
Michael <majekmichael3@gmail.com>
2024-03-28 17:18:08 +0000

    updated code

--- client/scripts/ingredients.mjs
+++ client/scripts/ingredients.mjs
@@ -6,27 +6,29 @@ let arrCopy = [];
 let allMeals = [];
 
 
-
 const ingredientButtons = document.querySelectorAll("button");
 
 ingredientButtons.forEach(function (ingredient) {
     ingredient.addEventListener('click', function (e) {
-        if (ingredientsArray.indexOf(ingredient.value) == -1) { //if the record does not pre-exist
-            
+        if (ingredientsArray.indexOf(ingredient.value) == -1) { //if the record does not pre-exist add to array and indent color
+
             ingredientsArray.push(ingredient.value);
             ingredient.style.border = "2px solid green"
         }
         else {
-            console.log(`${ingredient.value}, already exists`)
+            ingredientsArray.pop(ingredient.value)
+            ingredient.style.border = "none";
+            console.log(`${ingredient.value}, has been deselected`)
         }
     })
 })
 
 
-document.querySelector("#submitIngredientsButton").addEventListener('click', mealAlgorithm)
+document.querySelector("#submitIngredientsButton").addEventListener('click', getIngredientsList)
 
 // gonna make this an export function since its mjs.
 function getIngredientsList() {
+    console.log(`Selected Ingredients: ${ingredientsArray}`)
     return ingredientsArray;
 }
 
